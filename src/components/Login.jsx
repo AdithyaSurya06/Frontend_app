d02e96e4860421b844014d13f99266e34e5eb78b
AdithyaSurya06 <suryaadithya9849@gmail.com>
2026-03-13 11:45:40 +0530

    updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,42 +1,49 @@
-import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../App";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
-function Login() {
-  const { user, setUser } = useContext(AppContext);
+function Orders() {
   const API_URL = import.meta.env.VITE_API_URL;
-  const Navigate = useNavigate()
-  const handleLogin = async () => {
-    const url = API_URL + "/auth/signin";
-    const response = await axios.post(url, user);
-    setUser(response.data)
-    Navigate("/")
+  const { user } = useContext(AppContext);
+  const [orders, setOrders] = useState([]);
+
+  const fetchOrders = async () => {
+    try {
+      const url = `${API_URL}/orders/${user.email}`;
+      const response = await axios.get(url, {
+        headers: { Authorization: `Bearer ${user.token}` },
+      });
+      setOrders(response.data);
+    } catch (err) {
+      console.log("Something went wrong");
+    }
   };
+
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
   return (
     <div>
-      <h2>Login Page</h2>
-      <p>
-        <input
-          type="text"
-          onChange={(e) => setUser({ ...user, email: e.target.value })}
-          placeholder="Email"
-        />
-      </p>
-      <p>
-        <input
-          type="password"
-          onChange={(e) => setUser({ ...user, password: e.target.value })}
-          placeholder="Password"
-        />
-      </p>
-      <p>
-        <button onClick={handleLogin}>Login</button>
-      </p>
-      <p>
-        <Link to="/register">New user register here</Link>
-      </p>
+      <h1>My Orders</h1>
+      <div>
+        {orders &&
+          orders.map((order) => (
+            <div key={order._id}>
+              <h3>Order Id: {order.orderDate}</h3>
+              <ol>
+                {order.items.map((item) => (
+                  <li key={item._id}>
+                    {item.name}-{item.price}-{item.quantity}-
+                    {item.price * item.quantity}
+                  </li>
+                ))}
+              </ol>
+              <h3>Order Value: {order.orderValue}</h3>
+              <hr />
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
-export default Login;
+export default Orders;
